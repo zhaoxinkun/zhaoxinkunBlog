893a27ea2e45816d591f3a581f456bdc7572f0c5
赵小新 <zhaoxinkun1124@gmail.com>
2026-07-28 10:52:38 +0800

    chore: oxcfmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-    return (
-        <h1>Hello react</h1>
-    )
+  return <h1>Hello react</h1>;
 }
 
-export default App
+export default App;
