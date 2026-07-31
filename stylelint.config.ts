31ce4799ad4c32f22f405ef240718995e67cf980
赵小新 <zhaoxinkun1124@gmail.com>
2026-07-31 09:35:21 +0800

    chore: update stylelint

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,3 +1,12 @@
 export default {
   extends: ["stylelint-config-standard", "stylelint-config-recommended-scss"],
+  rules: {
+    // recommended rules
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    // ...
+    // any other rules you'd want to change e.g.
+    "scss/dollar-variable-pattern": "^foo",
+    "scss/selector-no-redundant-nesting-selector": true,
+  },
 };
